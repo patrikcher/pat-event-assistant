a8722db6425f2e9bf54498a4d06daf16d78ff801
patrikcher <patrickcher@gmail.com>
2018-03-04 16:32:01 +0800

    updated rss urls for debugging

--- index.php
+++ index.php
@@ -7,20 +7,22 @@ if($method == 'POST'){
 	$requestBody = file_get_contents('php://input');
 	
 	$json = json_decode($requestBody);
+	
+	$rss = 'https://www.nlb.gov.sg/golibrary2/events.rss';
 
 	$text = $json->result->parameters->eventType;
 
 	switch ($text) {
 		case 'programme':
-			$speech = "Hi, programme selected";
+			$speech = "Hi, programme selected. RSS URL is " + $rss + "?limit=8&type_id=1";
 			break;
 
 		case 'exhibition':
-			$speech = "Hi, exhibition selected";
+			$speech = "Hi, exhibition selected. RSS URL is " + $rss + "?limit=8&type_id=2";
 			break;
 
 		case 'conference':
-			$speech = "Hi, conference selected";
+			$speech = "Hi, conference selected. RSS URL is " + $rss + "?limit=8&type_id=3";
 			break;
 		
 		default:
